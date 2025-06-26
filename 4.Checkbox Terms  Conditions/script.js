a6c2c33c1fcf3ba170cb1fa874188982a1d1313a
Muzamil <muzamilzubair987654@gmail.com>
2025-06-26 19:48:24 +0500

    error remoe

--- 4.Checkbox Terms  Conditions/script.js
+++ 4.Checkbox Terms  Conditions/script.js
@@ -1,6 +1,6 @@
-const checkBox = document.getElementById('agree');
-const submitBtn = document.getElementById('btn');
+const checkBox = document.getElementById("agree");
+const submitBtn = document.getElementById("btn");
 
-submitBtn.addEventListener('change', ()=>{
-    submitBtn.disabled = ! checkBox.checked;
-})
+checkBox.addEventListener("change", () => {
+  submitBtn.disabled = !checkBox.checked;
+});
